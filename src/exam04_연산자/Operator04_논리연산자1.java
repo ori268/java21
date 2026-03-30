8c462e0a1ddf1185f9f941d238b418a85ca85510
ori268 <ojoj4055@naver.com>
2026-03-30 16:20:13 +0900

    test3-1 commit

---
+++ src/exam04_연산자/Operator04_논리연산자1.java
@@ -0,0 +1,35 @@
+package exam04_연산자;
+
+import java.awt.SystemColor;
+
+public class Operator04_논리연산자1 {
+
+	public static void main(String[] args) {
+		
+		// && : and 연산자
+		
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(false && false);
+
+		System.out.println("**********************************");
+		
+		// || : or 연산자
+		
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+	
+		System.out.println("**********************************");
+
+		// !! : not 연산자
+		
+		System.out.println(!true);
+		System.out.println(!false);
+				
+		
+	}
+
+}
---
+++ src/exam04_연산자/Operator05_증감연산자2.java
@@ -0,0 +1,18 @@
+package exam04_연산자;
+
+public class Operator05_증감연산자2 {
+
+	public static void main(String[] args) {
+
+		// 전치 연산자
+		int n = 10;
+		int result = ++n;   // 먼저 증가하고 나중에 할당
+		System.out.println(result +"\t" + n);
+
+		// 후치 연산자
+		int n2 = 10;
+		int result2 = n2++;    // 먼저 할당하고 나중에 증가
+		System.out.println(result2 +"\t" + n2);
+	}
+
+}
