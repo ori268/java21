9bcc2da9c311951edcce15faf9f05804b3327c67
ori268 <ojoj4055@naver.com>
2026-03-31 17:03:18 +0900

    day31-3 commit

---
+++ src/exam_07_배열/ArrayTest01_1차원배열2.java
@@ -0,0 +1,24 @@
+package exam_07_배열;
+
+public class ArrayTest01_1차원배열2 {
+
+	public static void main(String[] args) {
+	
+		// 이름을 배열로 관리 : 홍길동, 이순신, 유관순 관리하자
+		String names[];	
+		names = new String[3];  // null로 자동초기화
+	
+		System.out.println(names[0]);
+		System.out.println(names[1]);
+		System.out.println(names[2]);
+		
+		names[0]="홍길동";
+		names[1]="이순신";
+		names[2]="유관순";
+		
+		for(int i=0; i < names.length; i++) {
+			System.out.printf("names[%s]=%s \n", i, names[i]);
+		}
+	}
+
+}
