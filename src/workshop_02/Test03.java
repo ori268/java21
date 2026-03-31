d04d68ce7606d89beeee5233f16ccb4d7bd1aa8b
ori268 <ojoj4055@naver.com>
2026-03-31 17:44:45 +0900

    day31-5 commit

--- src/workshop_02/Test03.java
+++ src/workshop_02/Test03.java
@@ -4,7 +4,7 @@ public class Test03 {
 
 	public static void main(String[] args) {
 
-		// 잘모르겠음
+		// 잘모르겠음 여기까지 함.
 		char ch = 'z';
 		boolean b = ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z');
 		System.out.println(b);
