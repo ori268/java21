b3e6b85076935cc094dc4516ad993cf7d7c33e18
ori268 <ojoj4055@naver.com>
2026-04-01 15:37:23 +0900

    day4-1 commit

---
+++ src/workshop_03/Java21_워크샵03_문제_솔루션/Test05.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Test05 {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("정수를 입력하시오");
+		int num = scan.nextInt();
+		
+		int sum = 0;
+		double n = 0;
+		for(int i=1; i <= num; i++) {
+			if(i%2==0) {
+				System.out.println(i);
+				sum += i;
+				n++;
+			}
+		}
+		System.out.println("총합:" + sum);
+		System.out.println("평균값:" + sum/n);
+		
+		scan.close();
+	}
+}
